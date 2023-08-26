3919061985f0f2fb6637c8319426e47cd8ad3634
thailanddynamic <nextgendevoffice@gmail.com>
2023-08-26 18:21:19 +0700

    New

--- app.py
+++ app.py
@@ -1,70 +1,64 @@
+import os
 import requests
-import json
-from linebot import LineBotApi, WebhookHandler
+from linebot import LineBotApi
+from linebot.models import TextSendMessage, FlexSendMessage
 
-def get_live_scores():
-    url = "https://api.football-data.org/v2/matches/live"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = json.loads(response.content)
-        return data
-    else:
-        return None
+# ข้อมูลการเข้าถึง API Line
+LINE_CHANNEL_SECRET = os.environ['08c1695f0b7ecc5aba7ba36c10d0f281']
+LINE_CHANNEL_ACCESS_TOKEN = os.environ['8tR6Z7GkmuSg0Xwj1DGPkeBiSWKiP+D29jNlTejUh9hKVRaeWySDkdTD7D7IlIyHS2KTpq9zj92/jPT73Yo5l+mXH50HhqKQ3uCbF2Q9f9D4JRV14HxjnUP9OzGlPQlvk0Cs06+Yk5sa/e6ygJ9NJQdB04t89/1O/w1cDnyilFU=']
 
-def send_line_notification(data):
-    # ตรวจสอบค่าที่ส่งกลับจากฟังก์ชัน get_live_scores()
-    if data is not None:
-        for live_score in data:
-            home_team = live_score["homeTeam"]["name"]
-            away_team = live_score["awayTeam"]["name"]
-            score = live_score["score"]["fullTime"]
-            time = live_score["matchStats"]["elapsed"]
-            goals = live_score["goals"]
-            shots = live_score["shots"]
-            corners = live_score["corners"]
+# ข้อมูลการเข้าถึง API football-data.org
+FOOTBALL_DATA_API_KEY = os.environ['886eb5facd814d229f3ab17c41323a0d']
 
-            if score["homeScore"] != score["awayScore"]:
-                notification_message = f"[แจ้งเตือนผลบอลสด] {home_team} {score['homeScore']}-{score['awayScore']} {away_team} [นาทีที่ {time}]\n\n"
-                notification_message += f"ประตู\n"
-                for goal in goals:
-                    if goal["team"] == "home":
-                        goal_team = home_team
-                    elif goal["team"] == "away":
-                        goal_team = away_team
-                    else:
-                        goal_team = "UNKNOWN"
-                    notification_message += f"* {goal_time}: {goal_team} {goal['scorer']}\n"
-                notification_message += f"ยิงประตู\n"
-                for shot in shots:
-                    if shot["team"] == "home":
-                        shot_team = home_team
-                    elif shot["team"] == "away":
-                        shot_team = away_team
-                    else:
-                        shot_team = "UNKNOWN"
-                    notification_message += f"* {shot_time}: {shot_team} {shot['location']}\n"
-                notification_message += f"ลูกเตะมุม\n"
-                for corner in corners:
-                    if corner["team"] == "home":
-                        corner_team = home_team
-                    elif corner["team"] == "away":
-                        corner_team = away_team
-                    else:
-                        corner_team = "UNKNOWN"
-                    notification_message += f"* {corner_time}: {corner_team}\n"
-                line_bot_api.push_message(live_score["id"], notification_message)
-    else:
-        print("Error getting live scores")
-        
-def get_completed_matches():
-    url = "https://api.football-data.org/v2/matches/completed"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = json.loads(response.content)
-        return data
-    else:
-        raise Exception("Error getting completed matches")
+# ฟังก์ชันสำหรับรับข้อความจากผู้ใช้
+def handle_message(event):
+    # ตรวจสอบว่าผู้ใช้ส่งข้อความอะไรมา
+    if event.type == 'message':
+        # ตรวจสอบว่าผู้ใช้ส่งข้อความ /ตารางบอลวันนี้
+        if event.message.text == '/ตารางบอลวันนี้':
+            # ดึงข้อมูลตารางบอลวันนี้จาก API football-data.org
+            url = 'https://api.football-data.org/v2/matches/?date=today&competitions=all&status=SCHEDULED'
+            headers = {'X-Auth-Token': FOOTBALL_DATA_API_KEY}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                # แปลงข้อมูล JSON เป็นรายการ
+                matches = response.json()['matches']
+
+                # สร้าง Flex Message สำหรับแสดงตารางบอล
+                items = []
+                for match in matches:
+                    items.append({
+                        'type': 'text',
+                        'text': f'{match["utcDate"]} {match["homeTeamName"]} vs {match["awayTeamName"]}'
+                    })
+                flex_message = FlexSendMessage(
+                    alt_text='ตารางบอลวันนี้',
+                    template_type='carousel',
+                    contents=[
+                        {
+                            'type': 'text',
+                            'text': 'ตารางบอลวันนี้'
+                        },
+                        {
+                            'type': 'carousel',
+                            'contents': items
+                        }
+                    ]
+                )
+
+                # ส่งข้อความ Flex Message กลับไปยังผู้ใช้
+                line_bot_api.reply_message(event.reply_token, flex_message)
+
+# ฟังก์ชันหลักสำหรับรับข้อความจากผู้ใช้
+def main():
+    # เริ่มต้นใช้งาน LineBot
+    line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+    # ฟังก์ชันสำหรับรับข้อความจากผู้ใช้
+    line_bot_api.set_event_callback(handle_message)
+
+    # รอรับข้อความจากผู้ใช้
+    line_bot_api.run()
 
 if __name__ == "__main__":
-    live_scores = get_live_scores()
-    send_line_notification(live_scores)
+    main()
